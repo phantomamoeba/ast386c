c34d62828d62af65509cbf7453ae5dcea771104d
phantomamoeba <polonius@davistx.com>
2018-09-29 16:24:42 -0500

    initial hw2

---
+++ hw2/hw2_dd.py
@@ -0,0 +1,18 @@
+#AST 386C Properties of Galaxies
+#homework #2
+#October 16,2018
+#Dustin Davis
+
+
+import numpy as np
+from scipy.integrate import quad
+import math
+import os.path as op
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+import astropy.table
+
+
+BASEDIR = "res"
+OUTDIR = "out"
